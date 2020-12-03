3c9c93a09fe69eea26ee75ceedb62561c963233d
rabiacatakasnak <rabiaozturk19@hotmail.com>
2020-12-03 14:09:09 +0300

    Starts test beforeEach example

--- client/src/GuessedWords.js
+++ client/src/GuessedWords.js
@@ -3,7 +3,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const GuessedWords=(props)=> {
+  let contents
+  if(props.guessedWords.length===0){
 
+    contents=(
+      <span id="guessInstructions">
+        Try to guess the secret word
+      </span>
+    )
+  }
+  return(
+    <div id="componentGuessedWords">
+      {contents}
+    </div>
+  )
 }
 
 GuessedWords.propTypes={
--- client/src/GuessedWords.test.js
+++ client/src/GuessedWords.test.js
@@ -16,4 +16,26 @@ const setup=(props={})=>{
 
 test('does not throw warning with expected props',()=>{
   checkProps(GuessedWords,defaultProps)
+});
+
+describe('If there are no words guests',()=>{
+  let wrapper
+  beforeEach(()=>{
+    wrapper=setup({guessedWords: []})
+  });
+  test('render without error',()=>{
+    const component=findByTestAttr(wrapper,'componentGuessedWords')
+    expect(component.length).toBe(1)
+
+  });
+  test('renders instruction to guest a word',()=>{
+    const instructions=findByTestAttr(wrapper,'guessInstructions')
+    expect(instructions.text().length).not.toBe(0)
+
+  })
+
+});
+describe('If there are  words guests',()=>{
+
 })
+
